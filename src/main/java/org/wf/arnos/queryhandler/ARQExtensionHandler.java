7b42acd8795fcc429afe7ac1f444ad422ee144b5
cmcpb <cmcpb@ilrt-karath>
2010-09-14 16:21:08 +0100

    adding arq query handler to handle additional ARQ count(*) extension syntax

---
+++ src/main/java/org/wf/arnos/queryhandler/ARQExtensionHandler.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.wf.arnos.queryhandler;
+
+import com.hp.hpl.jena.query.Query;
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wf.arnos.controller.model.Endpoint;
+import org.wf.arnos.controller.model.sparql.Result;
+
+/**
+ *
+ * @author cmcpb
+ */
+public class ARQExtensionHandler extends ThreadedQueryHandler
+{
+    private static final Log LOG = LogFactory.getLog(ARQExtensionHandler.class);
+
+    /**
+     * This method handles a SELECT SPARQL query.
+     * It uses threads to query each endpoint and then combines the responses.
+     * @param query SPARQL SELECT query
+     * @param endpoints List of endpoints to query over
+     * @return Response string
+     */
+    @Override
+    public final String handleSelect(final String projectName, final Query query, final List<Endpoint> endpoints)
+    {
+        if (isCountQuery(query.toString())) return handleCountQuery(projectName, query, endpoints);
+        else return super.handleSelect(projectName, query, endpoints);
+    }
+
+    public String handleCountQuery(String projectName, Query query, List<Endpoint> endpoints) {
+        LOG.info("handling SELECT (count)");
+
+        List<Result> selectResultList = fetchResultSetAndWait(projectName, query, endpoints);
+
+        StringBuffer content = new StringBuffer(DEFAULT_SB_LENGTH);
+
+        content.append("<?xml version=\"1.0\"?><sparql xmlns=\"http://www.w3.org/2005/sparql-results#\"><head>");
+
+        // add head info
+        List<String> vars = query.getResultVars();
+        for (String var : vars)
+        {
+            content.append("<variable name=\"");
+            content.append(Result.escapeXMLEntities(var));
+            content.append("\"/>");
+        }
+        content.append("</head><results>");
+
+        // collate all responses
+        boolean hasLimit = false;
+        boolean distinct = false;
+        long limit = -1;
+
+        if (query.hasLimit())
+        {
+            limit = query.getLimit();
+            hasLimit = true;
+        }
+
+        if (query.isDistinct())
+        {
+            distinct = true;
+        }
+
+        if (query.hasOrderBy())
+        {
+            sortResults(selectResultList, query.getOrderBy());
+        }
+
+        if (query.hasAggregators())
+        {
+            int count = 0;
+            for (int i = 0; i < selectResultList.size(); i++)
+            {
+                Result r = selectResultList.get(i);
+                count += r.count;
+            }
+            content.append("<result><binding name=\""+ query.getResultVars().get(0).toString()+"\">");
+            content.append("<literal datatype=\"http://www.w3.org/2001/XMLSchema#integer\">"+count+"</literal>");
+            content.append("</binding></result>");
+        }
+        else
+        {
+            for (int i = 0; i < selectResultList.size(); i++)
+            {
+                Result r = selectResultList.get(i);
+                boolean add = true;
+
+                if (!hasLimit || limit > 0)
+                {
+                    if (hasLimit) limit--;
+                }
+                else
+                {
+                    add = false;
+                }
+
+                if (distinct)
+                {
+                    // check a duplicate result hasn't already been added
+                    boolean match = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (r.equals(selectResultList.get(j))) match = true;
+                    }
+                    if (match) add = false;
+                }
+
+                if (add) content.append(r.toXML());
+            }
+        }
+
+        content.append("</results></sparql>");
+
+        selectResultList.clear();
+
+        return content.toString();
+    }
+
+    /**
+     * Checks if given select query uses the count ARQ extension syntax
+     * @param s
+     * @return <code>true</codisCountQuerye> if query contains a count result, <code>false</code> otherwise
+     */
+    private boolean isCountQuery(String s)
+    {
+        if (s.toUpperCase().contains("COUNT")) return true;
+
+        return false;
+    }
+}
---
+++ src/test/java/org/wf/arnos/queryhandler/ARQExtensionHandlerTest.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.wf.arnos.queryhandler;
+
+import org.wf.arnos.utils.handler.EndpointHandler;
+import com.hp.hpl.jena.query.Syntax;
+import org.wf.arnos.utils.ARQSparql;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.wf.arnos.cachehandler.CacheHandlerInterface;
+import org.wf.arnos.cachehandler.SimpleCacheHandler;
+import org.wf.arnos.cachehandler.SimpleCacheHandlerTest;
+import org.wf.arnos.controller.QueryController;
+import org.wf.arnos.controller.model.Endpoint;
+import org.wf.arnos.controller.model.Project;
+import org.wf.arnos.controller.model.ProjectsManager;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.easymock.EasyMockSupport;
+import org.wf.arnos.utils.LocalServer;
+import java.io.StringWriter;
+import org.wf.arnos.utils.Sparql;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author cmcpb
+ */
+public class ARQExtensionHandlerTest extends EasyMockSupport
+{
+    Project p;
+    Endpoint ep1 = new Endpoint(Sparql.ENDPOINT1_URL); // 7 books
+    Endpoint ep2 = new Endpoint(Sparql.ENDPOINT2_URL); // 0 books
+    Endpoint ep3 = new Endpoint(Sparql.ENDPOINT3_URL); // 4 books
+    private QueryController controller;
+    static String PROJECT_NAME = "testproject";
+    static String QueryString = ARQSparql.ARQ_SELECT_COUNT;
+
+
+    @Before
+    public void setUp()
+    {
+        DOMConfigurator.configure("./src/main/webapp/WEB-INF/log4j.xml");
+        Logger.getLogger("org.wf.arnos.controller.QueryController").setLevel(Level.ALL);
+
+        ARQExtensionHandler queryHandler = new ARQExtensionHandler();
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.initialize();
+        queryHandler.setTaskExecutor(executor);
+
+        ProjectsManager manager = new ProjectsManager();
+
+        // setup default projects
+        p = new Project(PROJECT_NAME);
+        p.addEndpoint(Sparql.ENDPOINT1_URL);
+        p.addEndpoint(Sparql.ENDPOINT2_URL);
+        p.addEndpoint(Sparql.ENDPOINT3_URL);
+        manager.addProject(p);
+
+        controller = new QueryController();
+        controller.manager = manager;
+        controller.queryHandler = queryHandler;
+        controller.syntax = Syntax.syntaxARQ;
+
+        CacheHandlerInterface cache = null;
+        try
+        {
+            cache = new SimpleCacheHandler(new File(SimpleCacheHandlerTest.CACHE_SETTINGS));
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Throwing error");
+            ex.printStackTrace();
+            fail("Unable to create cache");
+        }
+
+        cache.flushAll(PROJECT_NAME);
+        controller.cacheHandler = cache;
+        QueryController.logger = LogFactory.getLog(QueryController.class);
+    }
+
+    @BeforeClass
+    public static void setUpClass()
+    {
+        LocalServer.handler = new EndpointHandler(new ARQSparql());
+        LocalServer.start();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        LocalServer.stop();
+    }
+
+    @Test
+    public void testARQExtension()
+    {
+        StringWriter writer = new StringWriter();
+        controller.executeQueryAcrossAllEndpoints(PROJECT_NAME, QueryString, writer);
+        String result = writer.getBuffer().toString();
+
+        System.out.println("result:"+result);
+        assertTrue("Counts tally",result.contains("467"));
+
+        // test output is valid xml
+        assertTrue("Is valid xml", Sparql.validateXML(result));
+    }
+
+
+}
